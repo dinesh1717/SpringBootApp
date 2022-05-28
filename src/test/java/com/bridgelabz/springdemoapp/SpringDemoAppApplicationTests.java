c6c83a7d8e4d586299dd0b18e01152a0f58bd10e
dinesh nangare <dineshnangare1717@gmail.com>
2022-05-28 23:56:43 +0530

    added controller

---
+++ src/test/java/com/bridgelabz/springdemoapp/SpringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
