e6788d9a76ab41e57a101446705c4dee4f74d2dc
dinesh nangare <dineshnangare1717@gmail.com>
2022-05-29 10:21:50 +0530

    added showing message by name

--- src/main/java/com/bridgelabz/springdemoapp/SpringDemoAppApplication.java
+++ src/main/java/com/bridgelabz/springdemoapp/SpringDemoAppApplication.java
@@ -8,6 +8,6 @@ public class SpringDemoAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoAppApplication.class, args);
+        System.out.println("Hello");
     }
-
 }
